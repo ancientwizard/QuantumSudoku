5ff43e9e60e7aa5985f6f80ac4742dae795d524a
Caleb Magoola <cmagoo01@villanova.edu>
2024-07-06 18:55:59 -0500

    Background Tiles for Starting Game

--- sudoku.js
+++ sudoku.js
@@ -45,6 +45,10 @@ function setGame() {
         for (let c = 0; c < 9; c++){
             let tile = document.createElement("div");
             tile.id = r.toString() + "-" + c.toString();
+            if (board[r][c] != "-"){
+                tile.innerText = board[r][c];
+                tile.classList.add("tile-start");
+            }
             tile.addEventListener("click", selectTile);
             tile.classList.add("tile");
             document.getElementById("board").append(tile);
